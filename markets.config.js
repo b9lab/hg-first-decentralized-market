7d6b45fcbc9c7fea2bfc40edd0d985cc80c57412
Xavier Lepretre <xavier.lepretre@gmail.com>
2019-08-07 18:57:12 +0200

    Created new markets for use by students.

--- markets.config.js
+++ markets.config.js
@@ -3,7 +3,7 @@ tomorrow.setDate(tomorrow.getDate() + 1);
 
 module.exports = [
   {
-    questionId: `0x${"0".repeat(63)}1`,
+    questionId: `0x${"0".repeat(63)}3`,
     title: `Will the share price of Tesla be above or below $300 at market close, ${tomorrow.toISOString()}.`,
     resolutionDate: tomorrow.toISOString(),
     outcomes: [
@@ -20,7 +20,7 @@ module.exports = [
     ]
   },
   {
-    questionId: `0x${"0".repeat(63)}2`,
+    questionId: `0x${"0".repeat(63)}4`,
     title: `Elon Musk will step down as CEO of Tesla on or before ${tomorrow.toISOString()}.`,
     resolutionDate: tomorrow.toISOString(),
     outcomes: [
